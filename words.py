a2966f2ece78c448a6acd4a3a1bcdfc460186a1f
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-01 17:23:05 +0500

    Update words.py

--- words.py
+++ words.py
@@ -7,12 +7,14 @@ class Words(Tokenize):
     """ делает токинезацию по словам """
     wordslist = []
     getstring = ""
+    onestring = ""
     def __init__(self, text):
         Tokenize.__init__(self, text)
 
     def load(self):
         """ токенизация по словам """
         self.getstring = Tokenize.load(self)
-        self.wordslist = re.split("(\w+|[a-z])", self.getstring)
+        self.onestring = re.sub("\.|\!|\,|\:|\;|\)|\(|\&|\#|\"|\?", "", self.getstring)
+        self.wordslist = re.split("(\w+|[a-z])", self.onestring)
         print(self.wordslist)
         return self.wordslist
