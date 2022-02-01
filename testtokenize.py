54752a4de64bf75adcb8241e23b2e13452d5395a
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-01 19:31:15 +0500

    Update testtokenize.py

--- testtokenize.py
+++ testtokenize.py
@@ -2,19 +2,12 @@ from sentences import Sentences
 from words import Words
 from punctuation import Punctuation
 
-tlist = []
-
 with open("data.txt", "r") as file:
-    for row in file:
-        if not row:
-            continue
-        else:
-            tlist.append(row)
-    varstring = "".join(tlist)
+    xstring = file.read()
 
-s = Sentences(varstring)
-w = Words(varstring)
-p = Punctuation(varstring)
-s.load()
-w.load()
-p.load()
+s = Sentences(xstring)
+w = Words(xstring)
+p = Punctuation(xstring)
+print(s.load())
+print(w.load())
+print(p.load())
