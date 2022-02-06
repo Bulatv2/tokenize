851b9c8fca1f73a4214f32e009a2c281b0bf64c4
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-06 18:19:39 +0500

    Update tokenize.py

--- tokenize.py
+++ tokenize.py
@@ -6,11 +6,13 @@ class Tokenize:
     """ подготавливает текст для токенизаций. """
     lowertext = ""
     clearstring = ""
+    onestring = ""
     def __init__(self, text):
         self.text = text
 
     def load(self):
         """ переводит текст в нижний регистр и заменяет \t\r\n символ " ". """
         self.lowertext = self.text.lower()
-        self.clearstring = re.sub("[\t\r\n]", " ", self.lowertext)
-        return self.clearstring
+        self.clearstring = re.sub("[\t\r\n\f\v]", "", self.lowertext)
+        self.onestring = re.sub("[ё]", "е", self.clearstring)
+        return self.onestring
