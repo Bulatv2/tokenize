b5891a331b700d68fa1dd072bb76bfaa54d3cfc3
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-13 07:30:38 +0500

    Update corpus.py

--- corpus.py
+++ corpus.py
@@ -1,10 +1,33 @@
-# module stopwords
+# module for stopwords, lemmatize, stemming
 
-xlist = []
+stoplist = []
+lemmadict = []
+stemdict = {}
+
 def stopwords(chlang):
     if chlang == "russian":
         with open("stopwordsru.txt", "r") as file:
-            xlist = file.read().split("\n")
-    return xlist
+            stoplist = file.read().split("\n")
+    return stoplist
 
-__version__ = "0.1"
+def lemma(chlang):
+    if chlang == "russian":
+        with open("lemmas.txt", "r") as file:
+            for line in file:
+                if not line:
+                    continue
+                else:
+                    left, right = line.split(":")
+                    lemmadict[left] = right
+    return lemmadict
+
+def stem(chlang):
+    if chlang == "russian":
+        with open("stems.txt", "r") as file:
+            for line in file:
+                if not line:
+                    continue
+                else:
+                    left, right = line.split(":")
+                    stemdict[left] = right
+    return stemdict
