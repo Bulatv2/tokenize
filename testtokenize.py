9de8ae60892101dd76bcf5d760e8243aa9a698f0
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-13 07:36:57 +0500

    Update testtokenize.py

--- testtokenize.py
+++ testtokenize.py
@@ -2,16 +2,41 @@ from sentences import Sentences
 from words import Words
 from punctuation import Punctuation
 import corpus
-
-with open("somedata.txt", "r") as file:
+import countvector
+"""
+with open("data.txt", "r") as file:
     xstring = file.read()
+"""
+llist = []
+rlist = []
+with open("data.txt", "r") as file:
+    for line in file:
+        if not line:
+            continue
+        else:
+            left, right, *res = line.split(":")
+            llist.append(left)
+            rlist.append(right)
+            xstring = " ".join(llist)
 stop = corpus.stopwords("russian")
-sent = Sentences(xstring)
+#stemming = corpus.stem("russian")
+#sent = Sentences(xstring)
 word = Words(xstring)
 w = word.load()
-punc = Punctuation(xstring)
-filtered = [str(x) for x in w if x not in stop]
-print(sent.load())
-print(word.load())
-print(filtered)
-print(punc.load())
+#punc = Punctuation(xstring)
+stopfiltered = [str(x) for x in w if x not in stop]
+#print(stopfiltered)
+vect = countvector.vector(llist)
+print(vect)
+"""
+for key, value in stemming.items():
+    for i in range(len(stopfiltered)):
+        if stopfiltered[i] == key:
+            stopfiltered[i] = value
+"""
+"""
+#print(sent.load())
+#print(word.load())
+print(stopfiltered)
+#print(punc.load())
+"""
