b8061162f9c6d42403acc82727dca1359781e63f
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-18 07:23:10 +0500

    Update lemmatize.py

--- lemmatize.py
+++ lemmatize.py
@@ -12,10 +12,14 @@ with open("lemmas.txt", "r") as file:
             left, right, *res = line.split(":")
             llist.append(left)
             rlist.append(right)
-
 class Lemmatize:
     def __init__(self, text):
         self.text = text
     def load(self):
-        self.lemmalist = [str(x) for x in self.text if x not in llist]
-        return self.lemmalist
+        for i in range(len(llist)):
+            for j in self.text:
+                if i == j:
+                    lemmalist.append(rlist[i])
+                else:
+                    lemmalist.append(j)
+            return lemmalist
