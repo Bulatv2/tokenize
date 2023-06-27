feb71c6a88d21c5b9dad67389bdeaa19f24f6e55
vbucode <bulatv2@gmail.com>
2023-06-28 00:07:39 +0500

    refactoring files

--- README.md
+++ README.md
@@ -5,24 +5,19 @@ tokenize to sentences, words, stop-words, stemming (ru)
 from nlp.words import Words
 from nlp.stem import Stemming
 from nlp.corpus import Corpus
-
 with open("testdata.txt", "r") as file:
     text = file.read()
-
 # tokenize to words
 tokword = Words(text)
 tokwordload = tokword.load()
 print("\nword tokenize:\n", tokwordload)
-
 # load stop words
 stop = Corpus()
 stopw = stop.stopwords("ru")
 stopfiltered = [x for x in tokwordload if x not in stop]
 print("\nwithout stop words:\n", stopfiltered)
-
 # load stemming form
 st = corpus.stemming()
-
 # stemming text
 stemming = Stemming(*st)
 stem = stemming.load(stopfiltered)
--- src/nlp/corpus.py
+++ src/nlp/corpus.py
@@ -2,18 +2,18 @@
 def stopwords(chlang):
     stoplist = []
     if chlang == "ru":
-        with open("/data/stopwordsru.txt", "r") as file:
+        with open("./data/stopwordsru.txt", "r") as file:
             stoplist = file.read().split("\n")
 
     if chlang == "eng":
-        with open("data/stopwordsru.txt", "r") as file:
+        with open("./data/stopwordsru.txt", "r") as file:
             stoplist = file.read().split("\n")
     return stoplist
 
 def stemming():
     llist = []
     rlist = []
-    with open("/data/stem.txt", "r") as file:
+    with open("./data/stem.txt", "r") as file:
         for line in file:
             if not line:
                 continue
